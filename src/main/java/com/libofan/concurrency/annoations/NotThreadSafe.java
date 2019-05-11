56ef86988f28014a234a1d95435ce3b7ecf7eb2b
libofan <libofan@bytedance.com>
2019-05-11 15:44:45 +0800

    first commit
    Change-Id: Ifa53a0d60ccba6a4ed8a4bf866bcd2367d28b598

---
+++ src/main/java/com/libofan/concurrency/annoations/NotThreadSafe.java
@@ -0,0 +1,12 @@
+package com.libofan.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.ANNOTATION_TYPE.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+    String value() default "";
+}
